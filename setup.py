4af4b3c66034d1a1a9d21219d448fcde93f66cbb
rauner <rauner@pik-potsdam.de>
2023-11-09 14:17:44 +0100

    add logger

--- setup.py
+++ setup.py
@@ -17,7 +17,9 @@ setup(
         'pandas>=1.4.0',
         'numpy>=1.21.0',
         'sqlalchemy>=1.4.0',
-        'scikit-learn>=1.0.0'
+        'scikit-learn>=1.0.0',
+        'python-json-logger>=2.0.7',
+        'kafka-python>=2.0.2',
     ],
     long_description=long_description,
     classifiers=[
