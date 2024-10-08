93266e54263c8b1806841c77e6ad59a146c861da
rauner <29773634+rauner@users.noreply.github.com>
2024-10-08 11:25:18 +0200

    remove kafka logger

--- buycycle/logger.py
+++ buycycle/logger.py
@@ -1,6 +1,5 @@
 import logging
 from pythonjsonlogger import jsonlogger
-from kafka.producer import KafkaProducer
 
 
 class AppJsonFormatter(jsonlogger.JsonFormatter):
@@ -31,28 +30,3 @@ class Logger:
         return logger
 
 
-class KafkaLogger(Logger):
-    def __init__(self, environment: str, ab: str, app_name: str, app_version: str, topic: str, bootstrap_servers: str, log_level=logging.INFO):
-        self.producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
-        self.topic = topic
-        self.logger = self.configure_logger(environment, ab, app_name, app_version, log_level)
-
-    def send_log(self, level, message, extra):
-        try:
-            log_record = self.logger.makeRecord(self.logger.name, level, None, None, message, None, None, extra={'extra': extra})
-            log_json = self.logger.handlers[0].formatter.format(log_record)
-            self.producer.send(self.topic, log_json.encode())
-        except Exception as e:
-            # Handle the exception (e.g., print to stderr, fallback to another logger, etc.)
-            print(f"Failed to send log to Kafka: {e}")
-    def debug(self, message, extra=None):
-        self.send_log(logging.DEBUG, message, extra)
-
-    def info(self, message, extra=None):
-        self.send_log(logging.INFO, message, extra)
-
-    def warning(self, message, extra=None):
-        self.send_log(logging.WARNING, message, extra)
-
-    def error(self, message, extra=None):
-        self.send_log(logging.ERROR, message, extra)
--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
         'sqlalchemy>=1.4.0',
         'scikit-learn>=1.0.0',
         'python-json-logger>=2.0.7',
-        'kafka-python>=2.0.2',
     ],
     long_description=long_description,
     classifiers=[
